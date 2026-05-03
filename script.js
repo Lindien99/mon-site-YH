af00618a8f1b7848b279a630da2cbfc174084ec8
Lindien99 <150619582+Lindien99@users.noreply.github.com>
2026-05-03 19:26:15 -0400

    Remplacer emojis drapeaux par images flagcdn.com (compatibilité Windows)

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const paysData = [
-    { nom: "Autriche",  code: "AUTRICHE",   emoji: "🇦🇹" },
-    { nom: "Belgique",  code: "BELGIQUE",   emoji: "🇧🇪" },
-    { nom: "Danemark",  code: "DANEMARK",   emoji: "🇩🇰" },
-    { nom: "Espagne",   code: "ESPAGNE",    emoji: "🇪🇸" },
-    { nom: "Finlande",  code: "FINLANDE",   emoji: "🇫🇮" },
-    { nom: "France",    code: "FRANCE",     emoji: "🇫🇷" },
-    { nom: "Grèce",     code: "GRECE",      emoji: "🇬🇷" },
-    { nom: "Hongrie",   code: "HONGRIE",    emoji: "🇭🇺" },
-    { nom: "Italie",    code: "ITALIE",     emoji: "🇮🇹" },
-    { nom: "Luxembourg",code: "LUXEMBOURG", emoji: "🇱🇺" },
-    { nom: "Norvège",   code: "NORVEGE",    emoji: "🇳🇴" },
-    { nom: "Pays-Bas",  code: "PAYS-BAS",   emoji: "🇳🇱" },
-    { nom: "Pologne",   code: "POLOGNE",    emoji: "🇵🇱" },
-    { nom: "Portugal",  code: "PORTUGAL",   emoji: "🇵🇹" },
-    { nom: "Suède",     code: "SUEDE",      emoji: "🇸🇪" },
-    { nom: "Suisse",    code: "SUISSE",     emoji: "🇨🇭" },
-    { nom: "Tchéquie",  code: "TCHEQUIE",   emoji: "🇨🇿" },
+    { nom: "Autriche",   code: "AUTRICHE",   iso: "at" },
+    { nom: "Belgique",   code: "BELGIQUE",   iso: "be" },
+    { nom: "Danemark",   code: "DANEMARK",   iso: "dk" },
+    { nom: "Espagne",    code: "ESPAGNE",    iso: "es" },
+    { nom: "Finlande",   code: "FINLANDE",   iso: "fi" },
+    { nom: "France",     code: "FRANCE",     iso: "fr" },
+    { nom: "Grèce",      code: "GRECE",      iso: "gr" },
+    { nom: "Hongrie",    code: "HONGRIE",    iso: "hu" },
+    { nom: "Italie",     code: "ITALIE",     iso: "it" },
+    { nom: "Luxembourg", code: "LUXEMBOURG", iso: "lu" },
+    { nom: "Norvège",    code: "NORVEGE",    iso: "no" },
+    { nom: "Pays-Bas",   code: "PAYS-BAS",   iso: "nl" },
+    { nom: "Pologne",    code: "POLOGNE",    iso: "pl" },
+    { nom: "Portugal",   code: "PORTUGAL",   iso: "pt" },
+    { nom: "Suède",      code: "SUEDE",      iso: "se" },
+    { nom: "Suisse",     code: "SUISSE",     iso: "ch" },
+    { nom: "Tchéquie",   code: "TCHEQUIE",   iso: "cz" },
 ];
 
 const grille = document.getElementById('grid-pays');
@@ -26,7 +26,7 @@ paysData.forEach(p => {
     carte.href = `pays/${p.code}.html`;
 
     carte.innerHTML = `
-        <div class="drapeau">${p.emoji}</div>
+        <div class="drapeau"><img src="https://flagcdn.com/w80/${p.iso}.png" alt="Drapeau ${p.nom}" width="80" height="53"></div>
         <div class="nom-pays">${p.nom}</div>
         <div style="margin-top:10px; font-size:0.8rem; color:#888;">Explorer le guide</div>
     `;
